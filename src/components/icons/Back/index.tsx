ccaab6ccbb962c0d1a7b35fa71a2f589be447928
ukeem <ikim.trader@gmail.com>
2025-04-02 21:06:45 +0500

    Update index.tsx

--- src/components/icons/Back/index.tsx
+++ src/components/icons/Back/index.tsx
@@ -8,7 +8,7 @@ interface BackProps {
 
 const Back: FC<BackProps> = ({ className, width = 20, height = 20 }) => {
 	return (
-		<svg className={className} width={width} height={height} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<svg className={className} width={width} height={height} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<g clipPath="url(#clip0_13712_507)">
 				<path d="M13 4L7 10L13 16" stroke="#3B3B3B" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
 			</g>
